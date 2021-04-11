0adc6d439bafc928fc3ae91663dc48a9f5b7ecbb
Agustin Di Nardo <agusdinar96@gmail.com>
2021-04-11 07:32:32 +0000

    Merged in feature/OT22-25-crear-migración-y-modelo-de-cate (pull request #17)
    Feature/OT22-25 crear migración y modelo de cate
    
    * created categories model and migration
    
    * added a seed
    
    Approved-by: Tomas Guzman

---
+++ seeders/20210406023624-demo-category.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Categories', [
+      {
+        name: 'Demo',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
