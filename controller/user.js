195198ea8fc941074582ddcc1f3a42110b8a19a6
Agustin Di Nardo <agusdinar96@gmail.com>
2021-04-12 23:57:29 +0000

    Merged in OT22-28-endpoint-de-eliminar-usuario (pull request #15)
    OT22-28 endpoint de eliminar usuario
    
    * Added delete user endpoint with the propper validations and user.js at /controller to make the queries to the db to get and delete by id
    
    * remove validateToken
    
    * Update const name in users route from User to user
    
    * Git pull from master and fixed conflicts
    
    Approved-by: Tomas Guzman

--- controller/user.js
+++ controller/user.js
@@ -3,5 +3,17 @@ const { User } = require('../models/index.js');
 module.exports = {
     getUserById: function (id) {
         return User.findByPk(id);
+    },
+
+    deleteUser: function (id) {
+        //Get current date in mysql format
+        let timeDeleted = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+        return User.update({ deletedAt: timeDeleted }, {
+            where: {
+                id
+            }
+        });
     }
+
 };
--- routes/users.js
+++ routes/users.js
@@ -11,30 +11,30 @@ const authMiddleware = require('../middlewares/auth');
 const authAdminMiddleware = require('../middlewares/authAdmin');
 
 /* POST register user */
-router.post('/auth/register', body('email').isEmail(), body('password').isLength({ min: 6 }), function(req, res, next){
+router.post('/auth/register', body('email').isEmail(), body('password').isLength({ min: 6 }), function (req, res, next) {
   const errors = validationResult(req);
   validate(errors)
   /* Encrypt password for send to BD */
   /* we are driver like a promise, in the finish the encrypt call to create and change the pass for the passhash*/
   hashPassword(req.body.password)
-  .then(r => user.create(req.body, req.body.password = r, res))
-  .then(r => res.send(r))
-  .catch(next)
+    .then(r => user.create(req.body, req.body.password = r, res))
+    .then(r => res.send(r))
+    .catch(next)
 
 })
 /* Function async hash */
-async function hashPassword(password){
+async function hashPassword(password) {
   const passwordHash = await bcrypt.hash(password, 10);
   return passwordHash
 }
 
 /* Function validate  */
-function validate (errors){
+function validate(errors) {
   if (!errors.isEmpty()) {
-    if(errors.errors.param == 'email'){
+    if (errors.errors.param == 'email') {
       return res.status(400).send('Email invalido');
     }
-    if(errors.errors.param === 'password'){
+    if (errors.errors.param === 'password') {
       return res.status(400).send('Password invalido');
     }
     return res.status(400).json({ errors: errors.array() });
@@ -46,16 +46,38 @@ router.get('/', authMiddleware, authAdminMiddleware, async (req, res, next) => {
   return res.status(200).json(userList);
 });
 
+router.delete('/:id', async (req, res) => {
+  let { id } = req.params;
+
+  try {
+    let checkUser = await user.getUserById(id);
+
+    if (checkUser) {
+
+      if (checkUser.dataValues.deletedAt != null) return res.status(400).json({ error: `User with id: ${id} doesn't exists` });
+
+      await user.deleteUser(id);
+
+      return res.status(200).json({ message: `User with id: ${id} deleted successfully` });
+    }
+
+    return res.status(400).json({ error: `User with id: ${id} doesn't exists` });
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+
+});
+
 module.exports = router;
 
 /* to login we are use the function compare, I get a example to future login */
-/* 
-  The hash come to BD 
+/*
+  The hash come to BD
   bcrypt.compare(password, passwordHasdeado, (err, coinciden) => {
-	  if (err) {
-	  	console.log("Error comprobando:", err);
-  	} else {
-		  console.log("¿La contraseña coincide?: " + coinciden);
-	  }
+    if (err) {
+      console.log("Error comprobando:", err);
+    } else {
+      console.log("¿La contraseña coincide?: " + coinciden);
+    }
   });
 */
