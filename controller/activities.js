89f74015ed303281a361e52941e12bbe8bc7e40e
Agustin Di Nardo <agusdinar96@gmail.com>
2021-04-19 22:38:49 +0000

    Merged in feature/OT22-51-endpoint-actualización-actividades (pull request #28)
    Feature/OT22-51 endpoint actualización actividades
    
    * created route and controller
    
    * fixed
    
    * fixed conflicts
    
    * fixed conflicts 2
    
    * fixed conflicts
    
    Approved-by: Tomas Guzman

--- controller/activities.js
+++ controller/activities.js
@@ -1,9 +1,18 @@
-const { Activities } = require('../models');
+const { Activities } = require('../models/');
 
 module.exports = {
+  findOne: (id) => {
+    return Activities.findByPk(id);
+  },
   create: (name, content, image) => {
     return Activities.create({ name, content, image }).then((act) => {
       return act.dataValues;
     });
+  },
+  edit: (id, name, content, image) => {
+    return Activities.update(
+      { name, content, image, updatedAt: new Date() },
+      { where: { id } }
+    );
   },
 };
--- routes/activities.js
+++ routes/activities.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const { body, validationResult } = require('express-validator');
-const activities = require('../controller/activities');
+const express = require('express');
+const router = express.Router();
+const { body, validationResult, param } = require('express-validator');
+const activity = require('../controller/activities');
 
 router.post(
   '/',
@@ -18,6 +19,36 @@ router.post(
       await activities.create(name, content, image);
 
       return res.status(201).json({ ok: true, msg: 'Created successfully' });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
+
+router.put(
+  '/:id',
+  param('id').isInt().notEmpty().trim().escape(),
+  body('name').isString().notEmpty().trim().escape(),
+  body('content').isString().notEmpty().trim().escape(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ ok: false, errors: errors.array() });
+      }
+
+      const { id } = req.params;
+      const { name, content, image } = req.body;
+
+      const response = await activity.findOne(id);
+
+      if (!response) {
+        return res.status(404).json({ ok: false, error: 'Activity not found' });
+      } else {
+        const edited = await activity.edit(id, name, content, image);
+        return res.status(200).json(edited);
+      }
     } catch (error) {
       console.log(error.message);
     }
