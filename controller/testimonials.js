22d929d1649aa5df9b83ba8a830f3d0fa1ea0a18
Daniel Palza <danielpalza@hotmail.com>
2021-04-19 23:58:50 +0000

    Merged in feature/Endpoint-Actualización-Testimonios (pull request #33)
    Feature/Endpoint Actualización Testimonios
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * updateTestimonials created in testimonials controller
    
    * findOneTestimonial created
    
    * /testimonials route config in app, testimonials/:id endpoint created
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * merge fix
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * fix merge conflict
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * fix merge conflict
    
    Approved-by: Tomas Guzman

--- controller/testimonials.js
+++ controller/testimonials.js
@@ -1,9 +1,23 @@
 const { Testimonials } = require('../models/index.js');
 
+ function updateTestimonial(data, id){
+  return Testimonials.update(data, {
+    where: {
+      id    }
+  });
+ 
+}
+function findOneTestimonial( id){
+   return Testimonials.findOne({ where: { id }});
+}
 
 function createTestimonials(data) {
   data.type = 'testimonials';
   return Testimonials.upsert(data);
 }
 
-module.exports.createTestimonials = createTestimonials;
+module.exports = { updateTestimonial, findOneTestimonial, createTestimonials};
+
+
+
+
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -2,7 +2,49 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-const { createTestimonials } = require('../controller/testimonials');
+const {
+  updateTestimonial,
+  findOneTestimonial,
+  createTestimonials
+} = require('../controller/testimonials');
+
+//PUT request to update the testimony
+router.put(
+  '/:id',
+  body('name').isString().notEmpty().trim().escape(),
+  body('content').isString().notEmpty().trim().escape(),
+  body('image').isString().notEmpty(),
+
+  async function (req, res) {
+    const errors = validationResult(req);
+    const data = req.body;
+    const id = req.params.id;
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ STATUS: 'ERROR', errors: errors.array() });
+    }
+
+    try {
+      if (await findOneTestimonial(id)) {
+        await updateTestimonial(data, id);
+        let response = await findOneTestimonial(id);
+
+        return res
+          .status(200)
+          .json({
+            Success: true,
+            msg: 'Update successfully.',
+            data: response.dataValues,
+          });
+      }
+
+      return res
+        .status(400)
+        .json({ Success: false, error: "Testimonial doesn't exist." });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+);
 
 // Post request for the creation of testimonials in data base 
 router.post(
