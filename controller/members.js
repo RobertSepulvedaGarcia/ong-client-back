99c49160229e62a5c2d4086f3867ff460a6752f5
Matias maizares <matiasmaizares2017@gmail.com>
2021-04-27 00:53:34 +0000

    Merged in feature/OT22-76-endpoint-eliminación-miembros (pull request #48)
    Feature/OT22-76 endpoint eliminación miembros
    
    * Arreglando conflictos
    
    * resolve conflicts
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * enpoint creation to remove members
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * fixing error in controller member
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-server
    
    * arreglando conflictos
    
    * conflictos
    
    Approved-by: Tomas Guzman

--- controller/members.js
+++ controller/members.js
@@ -1,14 +1,42 @@
-const { Members } = require('../models')
-
+const { Members } = require('../models');
 
 const getAllMembemres = () => {
-    return Members.findAll()
-}
+  return Members.findAll();
+};
 
 const createMember = (member) => {
-    return Members.create(member)
-}
-module.exports = { getAllMembemres, createMember }
+  return Members.create(member);
+};
+const deleteMember = async (req, res) => {
+  const existsMember = await Menbers.findOne(req.params.id);
 
+  if (!existsMember) {
+    return res.json({
+      msg: 'Member not exists',
+    });
+  }
 
+  try {
+    await Members.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    return res.json({
+      ok: true,
+
+      msg: 'Menber delete',
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.json({
+      ok: false,
+
+      msg: 'Error, try again',
+    });
+  }
+};
+
+module.exports = { getAllMembemres, createMember, deleteMember };
--- routes/members.js
+++ routes/members.js
@@ -1,32 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const { getAllMembemres, createMember } = require('../controller/members')
+const {
+  getAllMembemres,
+  createMember,
+  deleteMember,
+} = require('../controller/members');
 
 router.get('/', async (req, res, next) => {
-    try {
-        const result = await getAllMembemres()
-        res.status(200).json(result)
-    } catch (e) { next(e) }
-
-})
-router.post('/', body("name").notEmpty().isString(), body("image").isString(), async (req, res, next) => {
+  try {
+    const result = await getAllMembemres();
+    res.status(200).json(result);
+  } catch (e) {
+    next(e);
+  }
+});
+router.post(
+  '/',
+  body('name').notEmpty().isString(),
+  body('image').isString(),
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const { name, image } = req.body
-        const result = await createMember({ name, image })
-        if (result) {
-            return res.status(201).json({ message: 'Created Successfully' })
-        }
+      const { name, image } = req.body;
+      const result = await createMember({ name, image });
+      if (result) {
+        return res.status(201).json({ message: 'Created Successfully' });
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+router.delete('/members/:id', deleteMember);
 
-
-    } catch (e) { next(e) }
-
-
-
-})
-
-module.exports = router
+module.exports = router;
