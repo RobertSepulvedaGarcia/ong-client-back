47a39ed196a5ec94b2cc14274b8766c928d5d83e
Humberto Arambulo <hj.arambulo92@gmail.com>
2021-05-04 00:13:34 +0000

    Merged in feature/OT22-53-crear-seeders-para-guardar-activ (pull request #54)
    feature/OT22-53-crear-seeders-para-guardar-activ
    
    * modificado el seeder con actividades de demostracion para la ong
    
    Approved-by: Tomas Guzman

--- seeders/20210406132724-activity.js
+++ seeders/20210406132724-activity.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Activities', [
       {
-        name: 'Demo',
+        name: 'Activity 1',
         image:
           'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
         content:
@@ -12,6 +12,26 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      {
+        name: 'Activity 2',
+        image:
+          'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non leo ac diam tristique auctor nec et purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        name: 'Activity 3',
+        image:
+          'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non leo ac diam tristique auctor nec et purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
     ]);
   },
 
